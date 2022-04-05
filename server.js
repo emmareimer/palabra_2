d1d6b885ad45c92316f5318de42920b0977a269f
emma@beinproximity.com <emma@beinproximity.com>
2022-04-04 21:12:18 -0600

    added node modules

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const session = require('express-session');
 const routes = require('./controllers/index');
+const sequelize = require('./config/connection');
 // import sequelize connection
 
 const SequelizeStore = require('connect-session-sequelize')(session.Store);
