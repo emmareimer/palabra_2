6a78bfe6c50bbae6ef70b7b7df579ef375fca32d
emma@beinproximity.com <emma@beinproximity.com>
2022-04-04 20:21:01 -0600

    changed server.js to include auth, changed read me with more tasks

--- server.js
+++ server.js
@@ -1,16 +1,30 @@
 const express = require('express');
+const session = require('express-session');
 const routes = require('./controllers/index');
 // import sequelize connection
 
+const SequelizeStore = require('connect-session-sequelize')(session.Store);
+
 const app = express();
 const PORT = process.env.PORT || 3001;
+
+const sess = {
+  secret: process.env.SECRET,
+  cookie: {},
+  resave: false,
+  saveUninitialized: true,
+  store: new SequelizeStore({
+    db: sequelize
+  })
+};
+
+app.use(session(sess));
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 app.use(routes);
 
-// sync sequelize models to the database, then turn on the server
-app.listen(PORT, () => {
-  console.log(`App listening on port ${PORT}!`);
+sequelize.sync({ force: false }).then(() => {
+  app.listen(PORT, () => console.log('Now listening'));
 });
