4a4a9cb60259d03b0bc1ee196c1962bbe6af9978
emma@beinproximity.com <emma@beinproximity.com>
2022-04-06 11:08:13 -0600

    added logout route

--- controllers/html/index.js
+++ controllers/html/index.js
@@ -19,11 +19,29 @@ router.get('/profile', withAuth, async (req, res) => {
 });
 
 router.get('/', (req, res) => {
-    const isLoggedIn = req.session.logged_in || false
+    const isLoggedIn = req.session.logged_in || false;
     res.render('home', {isLoggedIn});
 });
 
 // TODO EMMA: Build past route handlebar /profile/word/{:ID}
+
+
+// TODO!!!!! WHY YOU NO WORK !!!!!!
+// Logout route
+// DELETE /api/auth/logout
+router.delete('/logout', (req, res) => {
+    if (req.session) {
+      req.session.destroy(err => {
+        if (err) {
+          res.status(400).send('Unable to log out')
+        } else {
+            res.redirect('/home')
+        }
+      });
+    } else {
+      res.end()
+    }
+  })
 
 
 module.exports = router;
