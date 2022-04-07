0687fca156d4bba2e28920a46502ea28d9c9fe3a
djmarcy <dj_marcy@hotmail.com>
2022-04-07 17:00:46 -0600

    incremental changes

--- server.js
+++ server.js
@@ -1,30 +1,31 @@
-const path = require('path'); 
-const express = require('express');
-const session = require('express-session');
-const routes = require('./controllers/index');
-const sequelize = require('./config/connection');
-const { engine } = require('express-handlebars');
+const path = require("path");
+const express = require("express");
+const session = require("express-session");
+const routes = require("./controllers/index");
+const sequelize = require("./config/connection");
+const { engine } = require("express-handlebars");
 
 // import sequelize connection
 
-const SequelizeStore = require('connect-session-sequelize')(session.Store);
+const SequelizeStore = require("connect-session-sequelize")(session.Store);
 
 const app = express();
 const PORT = process.env.PORT || 3001;
-const HOST = '0.0.0.0';
+const HOST = "0.0.0.0";
 const sess = {
-  secret: process.env.SECRET || 'secret',
+  secret: process.env.SECRET || "secret",
   cookie: {},
   resave: false,
   saveUninitialized: true,
   store: new SequelizeStore({
-    db: sequelize
-  })
+    db: sequelize,
+  }),
 };
 
-app.engine('handlebars', engine());
-app.set('view engine', 'handlebars');
-app.set('views', './views');
+app.engine("handlebars", engine());
+app.set("view engine", "handlebars");
+app.set("views", "./views");
+app.use(express.static("views"));
 
 app.use(session(sess));
 
@@ -32,10 +33,10 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 // ----------------- path to connect css, client side js to handlebars --------------------
-app.use(express.static(path.join(__dirname, '/public')));
+app.use(express.static(path.join(__dirname, "/public")));
 
 app.use(routes);
 
 sequelize.sync({ force: false }).then(() => {
-  app.listen(PORT, HOST, () => console.log('Now listening'));
+  app.listen(PORT, HOST, () => console.log("Now listening"));
 });
